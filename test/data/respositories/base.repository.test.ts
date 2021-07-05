d9774220d27ad454c5d7a99db3cdfda4b7392b9f
Ricardo Borges <ricardo.borges@somosphi.com>
2021-07-05 20:54:55 -0300

    setup test framework

---
+++ test/data/respositories/base.repository.test.ts
@@ -0,0 +1,37 @@
+import { Chance } from 'chance'
+import { model, Schema, Document } from 'mongoose'
+import { BaseRepository } from '../../../src/data/respositories/base.repository'
+
+const chance = new Chance()
+
+describe('BaseRepository', () => {
+  interface ITestModel extends Document {}
+  const testSchema = new Schema({})
+
+  const TestModel = model('User', testSchema)
+  class TestRepository extends BaseRepository<ITestModel> {}
+
+  let testRepository: TestRepository
+
+  beforeEach(() => {
+    testRepository = new TestRepository(TestModel)
+  })
+
+  describe('#find', () => {
+    it('should find() from model ', async () => {
+      TestModel.find = jest.fn().mockResolvedValue([{}])
+
+      await testRepository.find()
+
+      expect(TestModel.find).toBeCalledTimes(1)
+    })
+
+    it('should findOne() from model ', async () => {
+      TestModel.findById = jest.fn().mockResolvedValue({})
+      const id = chance.string({ numeric: true })
+      await testRepository.findById(id)
+
+      expect(TestModel.findById).toBeCalledTimes(1)
+    })
+  })
+})
